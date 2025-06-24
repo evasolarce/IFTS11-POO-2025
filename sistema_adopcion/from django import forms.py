12930e97b18355f0cf0b4efdd6e83709d84ebf0f
Evangelia Soledad Arce <evanarce@gmail.com>
2025-06-23 22:21:12 -0300

    modelos

--- sistema_adopcion/from django import forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Perro
-
-class PerroForm(forms.ModelForm):
-    class Meta:
-        model = Perro
-        fields = '__all__'
--- sistema_adopcion/models.py
+++ sistema_adopcion/models.py
@@ -12,3 +12,21 @@ class Perro(models.Model):
     vacunado = models.BooleanField(default=False)
     temperamento = models.CharField(max_length=100)
     estado = models.CharField(max_length=20, choices=[('disponible','Disponible'), ('reservado','Reservado'), ('adoptado','Adoptado')], default='disponible')
+
+
+class UsuarioAdoptante(models.Model):
+    nombre = models.CharField(max_length=100)
+    dni = models.CharField(max_length=20, unique=True)
+    email = models.EmailField()
+    preferencias = models.JSONField(default=dict)  # {'raza': ..., 'edad': ..., 'tamaño': ...}
+    historial_adopciones = models.ManyToManyField(Perro, blank=True, related_name='adoptantes')
+
+    def __str__(self):
+        return self.nombre
+    
+class SistemaAdopcion(models.Model):
+        perros = models.ManyToManyField(Perro, blank=True)
+        usuarios = models.ManyToManyField(UsuarioAdoptante, blank=True)
+
+        def __str__(self):
+            return "Sistema de Adopción"    
